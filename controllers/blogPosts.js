6fbb7f9f778221efe5a0449d535c54a45f7dcf3d
aladintrap <chiper.adelin01@gmail.com>
2022-11-24 01:55:07 +0200

    Blog added with author bug

--- controllers/blogPosts.js
+++ controllers/blogPosts.js
@@ -2,13 +2,25 @@ const db = require("../db-config");
 const e = require("express");
 
 const blogPosts = (req, res) => {
-    db.query("SELECT * FROM posts WHERE user_id = ?", [req.user.id], (err, result) => {
+
+    db.query("SELECT * FROM posts", (err, articles) => {
         if(err) throw err;
-        if(!result.length){ 
-            res.render("blog", { title: 'REGISTER URZISOFT', layout: './layouts/home' });
+        if(!articles.length){ 
+            res.render('blog', { title: 'URZI-BLOG', layout: './layouts/home', message: "Articles no more avaible!" })
+        }
+        else {
+            
+            db.query("SELECT * FROM users WHERE id = ?", [articles[0].user_id], (err, user) => {
+                if(err) throw err;
+                console.log([articles[3].user_id]);
+                let username = user[0].username;
+            
+            res.render('blog', { title: 'URZI-BLOG', layout: './layouts/home', message: "Doua articole disponibile!", articles, username })
+            })
         }
     })
-}
+}   
+
 
 
 module.exports = blogPosts;
